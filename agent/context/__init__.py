08caadbc051fa17b95bbc0e474d3a83acf4b2152
Nicholas Pitzarella <pkbrvhj5pn@privaterelay.appleid.com>
2026-01-19 06:08:57 -0500

    import change for structs removal

--- agent/context/__init__.py
+++ agent/context/__init__.py
@@ -1,7 +1,7 @@
 from .coordinator import ContextCoordinator
 from .store import ContextStore
 from .file_tracker import FileTracker
-from .structs import Message, ContextStats
+from protocol_monk.agent.structs import Message, ContextStats
 
 __all__ = [
     "ContextCoordinator",
